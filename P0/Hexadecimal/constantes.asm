3d37ccb809240f671d056aee598e0728bcc493d4
eduzc <ej.zeballos.2017@alumnos.urjc.es>
2021-07-03 11:03:01 +0200

    teclado hexadecimal con subrutinas

---
+++ P0/Hexadecimal/constantes.asm
@@ -0,0 +1,47 @@
+# Constantes
+
+	.eqv BASE 0xFFFF0010
+	.eqv DIG_0 0x3F
+	.eqv DIG_1 0x06
+	.eqv DIG_2 0x5B
+	.eqv DIG_3 0x4F
+	.eqv DIG_4 0x66
+	.eqv DIG_5 0x6D
+	.eqv DIG_6 0x7D
+	.eqv DIG_7 0x07
+	.eqv DIG_8 0x7F
+	.eqv DIG_9 0x6F
+	.eqv DIG_A 0x77
+	.eqv DIG_B 0x7C
+	.eqv DIG_C 0x39
+	.eqv DIG_D 0x5E
+	.eqv DIG_E 0x79
+	.eqv DIG_F 0x71
+	.eqv DISP_L 01  #-- BASE + 1
+	
+	#-- Seleccion de la fila
+	.eqv KEY_ENA 02  #-- BASE + 2
+	
+	#-- Lectura del codigo de tecla
+	.eqv KEY_RD  04  #-- BASE + 4
+
+	# Constantes
+
+	.eqv num_0 0x00000011
+	.eqv num_1 0x00000021
+	.eqv num_2 0x00000041
+	.eqv num_3 0xffffff81
+	.eqv num_4 0x00000012
+	.eqv num_5 0x00000022
+	.eqv num_6 0x00000042
+	.eqv num_7 0xffffff82
+	.eqv num_8 0x00000014
+	.eqv num_9 0x00000024
+	.eqv num_A 0x00000044
+	.eqv num_B 0xffffff84
+	.eqv num_C 0x00000018
+	.eqv num_D 0x00000028
+	.eqv num_E 0x00000048
+	.eqv num_F 0xffffff88
+
+

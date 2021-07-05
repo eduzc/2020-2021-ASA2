32f1e480c6bb99edd95edb757f821f8be88e1fe2
eduzc <ej.zeballos.2017@alumnos.urjc.es>
2021-07-05 19:49:41 +0200

    depurando codigo

--- P0/7SEGCONT2/main.asm
+++ P0/7SEGCONT2/main.asm
@@ -6,7 +6,7 @@
 	.data
 
 	
-palabras: .word LED_D, DIG_1, DIG_2, DIG_3
+palabras: .word LED_D, DIG_0, DIG_1, DIG_2, DIG_3, DIG_4, DIG_5, DIG_6, DIG_7, DIG_8, DIG_9
 	
 	.text
 	la x5, palabras #acceso a puntero
